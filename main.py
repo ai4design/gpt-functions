17c88129febfc2b8050a67840cab9ba92daa69cd
Marcus Bergo <eu@mbergo.dev>
2023-09-01 00:25:04 -0300

    Example of LangChaing and GPT Functions

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import json
 import openai
-from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage, AIMessage, ChatMessage
